3417d64249b27e2ee718e9fc8cdbe5c333d704f7
SaadNasir92 <nas.saad53@gmail.com>
2024-05-28 10:14:46 -0500

    Verified work, final commit

--- EmployeeSQL/analysis_employees.sql
+++ EmployeeSQL/analysis_employees.sql
@@ -5,14 +5,12 @@ select e.emp_no as "Employee Number", e.last_name as "Last Name" , e.first_name 
 from employees e left join salaries s on e.emp_no = s.emp_no
 ;
 
-
 drop view if exists Hired_in_1986;
 -- View for Employees hired in 1986.
 create view Hired_in_1986 as
 select e.first_name as "First Name" , e.last_name as "Last Name" , e.hire_date as "Hire Date"  from employees e 
 where extract(year from e.hire_date) = 1986
 ;
-
 
 drop view if exists Department_Managers;
 --View for Manager of each department.
@@ -23,7 +21,6 @@ left join manager_departments md on d.dept_no = md.dept_no
 left join employees e on md.emp_no = e.emp_no 
 ;
 
-
 drop view if exists Department_Employees;
 -- View for employees with department number.
 create view Department_Employees as
@@ -32,7 +29,6 @@ from employees e
 left join employee_departments ed on e.emp_no = ed.emp_no 
 left join departments d on ed.dept_no = d.dept_no 
 ;
-
 
 -- Employees whose first name is Hercules and last name begins with b.
 select * from employees e 
@@ -60,17 +56,10 @@ where
 d.dept_name = 'Sales' or d.dept_name = 'Development'
 ;
 
-
 drop view if exists same_last_name_employee_counts;
 -- View of same last name employee count
 create view same_last_name_employee_counts as
-select last_name, count(*)  from employees e 
+select last_name, count(*) as total  from employees e 
 group by last_name 
-order by last_name desc 
+order by total desc 
 ;
-
-
-
-
-
-
